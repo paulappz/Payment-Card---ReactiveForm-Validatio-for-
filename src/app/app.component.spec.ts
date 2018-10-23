1727aaf6eafd32508bad3fea989e1aa46dcaf768
paulappz <pauloluyege@gmail.com>
2018-10-23 17:33:05 +0100

    test passed @paulappz

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,25 +11,67 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpModule
+      ],
+      providers: []
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+
+  it('form should be invalid when empty', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const app = fixture.componentInstance;
+    expect(app.paymentForm.valid).toBeFalsy();
   });
 
-  it(`should have as title 'Vanhack'`, () => {
+  it('card number validation should work', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Vanhack');
-  });
+    const app = fixture.componentInstance;
+    app.paymentForm.controls['cardNumber'].setValue("1234123412345678");
+    expect(app.paymentForm.controls['cardNumber'].valid).toBeTruthy();
+  }));
 
-  it('should render title in a h1 tag', () => {
+  it('expiration date validation should work', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Vanhack!');
-  });
+    const app = fixture.componentInstance;
+    app.paymentForm.controls['expirationMonth'].setValue("134");
+    app.paymentForm.controls['expirationYear'].setValue("2017");
+    expect(app.paymentForm.controls['expirationMonth'].valid).toBeFalsy();
+    expect(app.paymentForm.controls['expirationYear'].valid).toBeTruthy();
+  }));
+
+  it('cvv/cvc validation should work', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.paymentForm.controls['cvv'].setValue("677");
+    expect(app.paymentForm.controls['cvv'].valid).toBeTruthy();
+  }));
+
+  it('submit button should display the success message', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.paymentForm.controls['nameOnCard'].setValue("Mathew Jerome");
+    app.paymentForm.controls['cardNumber'].setValue("3456234567854567");
+    app.paymentForm.controls['expirationMonth'].setValue("11");
+    app.paymentForm.controls['expirationYear'].setValue("2017");
+    app.paymentForm.controls['cvv'].setValue("677");
+    app.submitForm();
+    expect(app.displayMessage).toBe("Payment Successful!");
+  }));
+
+  it('submit button should display the failure message', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.paymentForm.controls['nameOnCard'].setValue("Mathew Jerome");
+    app.paymentForm.controls['cardNumber'].setValue("345623456784567");
+    app.paymentForm.controls['expirationMonth'].setValue("15");
+    app.paymentForm.controls['expirationYear'].setValue("2017");
+    app.paymentForm.controls['cvv'].setValue("6776");
+    app.submitForm();
+    expect(app.displayMessage).toBe("Payment Failed!");
+  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,13 @@ export class AppComponent {
   constructor(private formBuilder: FormBuilder) {
     /* Declare Reactive Form Group here */
     this.paymentForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.pattern("^[a-zA-Z ]+$")]],
+      nameOnCard: ['', [Validators.required, Validators.pattern("^[a-zA-Z ]+$")]],
       cardNumber: ['', [Validators.required, Validators.pattern("^[0-9]{16}$")]],
       // expirationMonth: ['',[Validators.required,Validators.max(2),Validators.pattern("^[0-9]{2}$")]],
       expirationMonth: ['', [Validators.required, Validators.maxLength(2), Validators.pattern("^(0?[1-9]|1[012])$")]],
       // expirationYear : ['',[Validators.required,Validators.max(4),Validators.pattern("^[0-9]{4}$")]],
       expirationYear: ['', [Validators.required, Validators.maxLength(4), Validators.pattern("^[0-9]{4}$")]],
-      cvvCvcNumber: ['', [Validators.required, Validators.pattern("^[0-9]{3}$")]],
+      cvv: ['', [Validators.required, Validators.pattern("^[0-9]{3}$")]],
     });
 
   }
